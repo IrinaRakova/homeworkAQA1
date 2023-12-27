f4085159b2ea9d26d889ff77728d1de21e250087
Ирина Ракова <irakova.education@gmail.com>
2023-12-27 20:23:00 +0300

    Add CachbackHackService tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,73 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldCalculateIfAmountLessBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+
+        int actual = service.remain(amount);
+        int expected = 200;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountMoreBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2750;
+
+        int actual = service.remain(amount);
+        int expected = 250;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountEquals1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountEquals999() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountEquals0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountEquals1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(expected, actual);
+    }
+}
